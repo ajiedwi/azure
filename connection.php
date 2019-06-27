44ec6c54a7be53821e05f19beb10bfdfc2468ff6
ajie47 <ajiedwi4798@gmail.com>
2019-06-27 14:54:06 +0700

    coba db

--- connection.php
+++ connection.php
@@ -14,32 +14,9 @@
 	// } catch(PDOException $e) {
 	//     echo "Failed: " . $e;
 	// }
-	try{
-		$connectionInfo = array("UID" => "ajie@ajiedwi", "pwd" => "@jie4798", "Database" => "ajiedwi", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-		$serverName = "tcp:ajiedwi.database.windows.net,1433";
-		$conn = sqlsrv_connect($serverName, $connectionInfo);	
-		$sql_select = "SELECT * FROM Registration";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
-            }
-	} catch (Exception $e){
-		echo $e;
-	}
+	$connectionInfo = array("UID" => "ajie@ajiedwi", "pwd" => "@jie4798", "Database" => "ajiedwi", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:ajiedwi.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);	
+
 	
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 
 include 'connection.php'
+try {
+            $sql_select = "SELECT * FROM Registration";
+            $stmt = $conn->query($sql_select);
+            $registrants = $stmt->fetchAll(); 
+            if(count($registrants) > 0) {
+                echo "<h2>People who are registered:</h2>";
+                echo "<table>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
+                foreach($registrants as $registrant) {
+                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['date']."</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h3>No one is currently registered.</h3>";
+            }
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
     // $sql_select = "SELECT * FROM user";
     // try{
     //     $stmt = $conn->query($sql_select);
