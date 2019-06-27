7e13c0963c8646ebd44b7cedb8a3fcd3901e3955
ajie47 <ajiedwi4798@gmail.com>
2019-06-27 14:27:39 +0700

    coba db

--- connection.php
+++ connection.php
@@ -17,8 +17,7 @@
 	try{
 		$connectionInfo = array("UID" => "ajie@ajiedwi", "pwd" => "@jie4798", "Database" => "ajiedwi", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 		$serverName = "tcp:ajiedwi.database.windows.net,1433";
-		$conn = sqlsrv_connect($serverName, $connectionInfo);
-		echo 'db sukses';	
+		$conn = sqlsrv_connect($serverName, $connectionInfo);	
 	} catch (Exception $e){
 		echo $e;
 	}
