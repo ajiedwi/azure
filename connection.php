b3946d75b0eeb0228839e58a3a4fc95d54f3c2bb
ajie47 <ajiedwi4798@gmail.com>
2019-06-27 14:07:05 +0700

    fix db

--- connection.php
+++ connection.php
@@ -8,10 +8,13 @@
 	$user = "ajie";
 	$pass = "@jie4798";
 	$db = "dbo.user";
-	try {
-	    $conn = new PDO("sqlsrv:server = tcp:ajiedwi.database.windows.net,1433; Database = ajiedwi", "ajie", "@jie4798");
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	} catch(PDOException $e) {
-	    echo "Failed: " . $e;
-	}
+	// try {
+	//     $conn = new PDO("sqlsrv:server = tcp:ajiedwi.database.windows.net,1433; Database = ajiedwi", "ajie", "@jie4798");
+ //    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// } catch(PDOException $e) {
+	//     echo "Failed: " . $e;
+	// }
+	$connectionInfo = array("UID" => "ajie@ajiedwi", "pwd" => "{your_password_here}", "Database" => "ajiedwi", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:ajiedwi.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
